3989384330578a85886b36772e6da616d690a1d2
Alysson Brendo Araujo Canuto <alyssonestud@hotmail.com>
2026-05-22 13:23:04 +0100

    1
    1

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030ff334368787e32eaa204998b620f5ad02c5f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35e0563d8bfcb4ec5e472601fd6a014fa79b22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
